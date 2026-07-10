70e5ab448b788db2175f8331bc0a779346abeb45
nahii-bl <nahomabrham49@gmail.com>
2026-07-10 10:32:46 +0300

    Update and rename .SCRIPT to 1script.js

--- 1script.js
+++ 1script.js
@@ -2,12 +2,13 @@ const supabaseUrl = "https://ufzdbyrcflftpupqqzjg.supabase.co";
 const supabaseKey = "sb_publishable_8169Y0pYnw0LtF206Ra9OA_QEoTU5Cc";
 
 const supabase = window.supabase.createClient(
+    
     supabaseUrl,
     supabaseKey
 );
 
 
-// Customer Request
+// Customer Request 
 const contactForm = document.querySelector(".contact form");
 
 if (contactForm) {
